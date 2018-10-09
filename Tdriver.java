4f9fac8226dc026e3102ae4bad5aa2b3fa675943
psspratik <psspratik@gmail.com>
2018-10-09 22:00:41 +0530

    Add files via upload

---
+++ Tdriver.java
@@ -0,0 +1,43 @@
+package classdemob2;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+
+public class Tdriver extends Configured implements Tool{
+      public int run(String[] args) throws Exception
+      {
+            
+            JobConf conf = new JobConf(getConf(), Tdriver.class);
+            conf.setJobName("HighestDriver");
+
+         
+            conf.setOutputKeyClass(Text.class);
+            conf.setOutputValueClass(IntWritable.class);
+
+            
+            conf.setMapperClass(Tmapper.class);
+            conf.setReducerClass(Treducer.class);
+            
+            Path inp = new Path(args[0]);
+            Path out = new Path(args[1]);
+            
+            FileInputFormat.addInputPath(conf, inp);
+            FileOutputFormat.setOutputPath(conf, out);
+
+            JobClient.runJob(conf);
+            return 0;
+      }
+     
+      public static void main(String[] args) throws Exception
+      {
+           
+        int res = ToolRunner.run(new Configuration(), new Tdriver(),args);
+            System.exit(res);
+      }
+}
+
+
---
+++ Treducer.java
@@ -0,0 +1,34 @@
+package classdemob2;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class Treducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>
+{
+      
+      public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException
+      {
+    	  int max_temp = 0; 
+    	  ; 
+          while (values.hasNext())
+                      {
+        	  int current=values.next().get();
+                         if ( max_temp <  current)  
+                        	 max_temp =  current;
+                      }
+          output.collect(key, new IntWritable(max_temp/10));
+
+      }
+
+	
+		
+	
+      
+}
+      
+      
+      
+      
+     
